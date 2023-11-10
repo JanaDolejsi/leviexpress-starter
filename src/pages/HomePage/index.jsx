9aa3833bf493d3ea34ae151295a65a93145eea30
Martin Podloucký <podlouckymartin@gmail.com>
2023-11-10 14:48:15 +0100

    Příprava HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,23 +1,9 @@
-import './style.css';
+import { JourneyPicker } from '../../components/JourneyPicker';
 
 export const HomePage = () => {
   return (
-    <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
-    </div>
+    <main>
+      <JourneyPicker />
+    </main>
   );
 };
